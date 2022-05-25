43373d52be8e6811e73027f4cc1e0ac888095934
abdul <abdul.tanveer@gmail.com>
2022-05-25 09:56:11 +0530

    starting another activity

--- app/src/main/java/com/abdul/b2harmanandroid/HomeActivity.kt
+++ app/src/main/java/com/abdul/b2harmanandroid/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.abdul.b2harmanandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,10 @@ class HomeActivity : AppCompatActivity() {
         var abdul = Student("ansari",123,true)
         abdul.name = "ansari"
         var emp = Employee("abdul",123,false)
+    }
+
+    fun clickHandler(view: View) {
+        var hIntent = Intent(this, MainActivity::class.java)
+        startActivity(hIntent)
     }
 }
