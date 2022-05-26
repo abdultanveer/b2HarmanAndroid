45b22257d1e330aa80a6d99704caefdf6b1cc5b0
abdul <abdul.tanveer@gmail.com>
2022-05-26 12:28:10 +0530

    Recyclerview - part 2 --creating adapter

--- app/src/main/java/com/abdul/b2harmanandroid/RecyclerActivity.java
+++ app/src/main/java/com/abdul/b2harmanandroid/RecyclerActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerActivity extends AppCompatActivity {
-    String[] countries = new String[]{"india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan"};
+    String[] countries = new String[]{"india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan",
+            "india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan",
+            "india","saudi","iran","afghanistan","india","saudi","iran","afghanistan","india","saudi","iran","afghanistan"};
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -19,5 +22,8 @@ public class RecyclerActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         RecyclerView cRecyclerView = findViewById(R.id.recyclerView);
+        CountriesAdapter adapter = new CountriesAdapter(countries);
+        cRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cRecyclerView.setAdapter(adapter);
     }
 }
