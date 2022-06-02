1fdf380d7b6b8d0a44ea4437c0f6ff42f14053db
abdul <abdul.tanveer@gmail.com>
2022-06-02 11:20:56 +0530

    who wrote it-1

---
+++ app/src/main/java/com/abdul/b2harmanandroid/WhoWroteItActivity.java
@@ -0,0 +1,18 @@
+package com.abdul.b2harmanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WhoWroteItActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_who_wrote_it);
+    }
+
+    public void searchBooks(View view) {
+    }
+}
